381e7e93cea6b667294e34574033d0cdf50b59a7
Componly <117368889+Componly-co@users.noreply.github.com>
2024-01-25 14:23:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,33 +3,7 @@
 const withComponly = require('@componly/nextjs-plugin').default;
 
 const nextConfig = {
-  reactStrictMode: true,
-    async headers() {
-    return [
-      {
-        // Set CORS headers
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Origin, X-Requested-With, Content-Type, Accept',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true
 }
 
 module.exports = withComponly(nextConfig); 
